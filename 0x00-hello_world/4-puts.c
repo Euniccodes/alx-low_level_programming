5f7365a5554da4fadb8d95d556990d7d27f48b74
Euniccodes <opuruiche4u@gmail.com>
2022-09-10 12:17:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+
+
+
 /**
-*
-*
-*Return: Always 0 (Successful)
-*/
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
 int main(void)
+  
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-    }
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+  
+}
 

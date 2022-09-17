ba19ee69f432fe855d25dfec7e3731a4ba3c7d6c
Euniccodes <opuruiche4u@gmail.com>
2022-09-16 21:34:40 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+/**
+
+ * _isupper - checks for uppercase character
+
+ * @c: the chcks input of function
+
+ * Return: 1 if c is uppercase, 0 otherwise
+
+ */
+
+
+
+int _isupper(int c)
+  
+{
+  
+  return (c >= 65 && c <= 90)
+  return (1);
+  return (0);
+  
+}

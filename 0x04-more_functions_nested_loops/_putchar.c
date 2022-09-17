140a7023b57ebfad35e2477f4e86086b43df7909
Euniccodes <opuruiche4u@gmail.com>
2022-09-16 22:12:42 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,14 +1,18 @@
 #include <unistd.h>
-#include "main.h"
+
+
 
 /**
 
  * _putchar - writes the character c to stdout
 
- * @c: the character to print
+ * @c: The character to print
+
+ *
 
  * Return: On success 1.
- *On error, -1 is returned, and error is set appropriately.
+
+ * On error, -1 is returned, and errno is set appropriately.
 
  */
 

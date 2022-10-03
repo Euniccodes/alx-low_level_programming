cf209e7815a224f3162644b898d0a3b0345e7734
Uchechi Eunice Owunne <93526806+Euniccodes@users.noreply.github.com>
2022-10-03 09:31:00 +0100

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,15 @@
-tot
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 through 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; ++i)
+		_putchar(i);
+	_putchar('\n');
+}

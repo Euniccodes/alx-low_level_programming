de87b836dc0cad925f1048078f4a0cfbc2a34894
Uchechi Eunice Owunne <93526806+Euniccodes@users.noreply.github.com>
2022-10-03 09:25:42 +0100

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,13 @@
-mmm
+#include "main.h"
+
+/**
+ * mul - multiply two numbers
+ * @a: first multiplication operand
+ * @b: second multiplication operand
+ *
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

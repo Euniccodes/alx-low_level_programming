c3ff1cb03338b030c8597c9c2e44060f36a1976b
Uchechi Eunice Owunne <93526806+Euniccodes@users.noreply.github.com>
2022-10-25 16:33:14 +0100

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,17 @@
-utc
+#include "lists.h"
+/**
+ *free_listint- free's listint_t
+ *@head: a ptr tp the listint_t list
+ */
+void free_listint(listint_t *head)
+{
+	 listint_t *temp;
+
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}

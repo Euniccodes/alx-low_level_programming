4cd0cfefe44e72ea29a4107a271addf28d513682
Uchechi Eunice Owunne <93526806+Euniccodes@users.noreply.github.com>
2022-10-26 05:45:02 +0100

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,17 @@
-bbn
+#include "lists.h"
+/**
+ *sum_listint- gives the sum of of all the data.
+ *@head: a ptr the listint_t list
+ *Return: the sum of the data if it is empty 0.
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}

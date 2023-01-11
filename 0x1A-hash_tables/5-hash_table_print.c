a47eb9c09e27a54a55791d98cb7056d35a1e68e1
Uchechi Eunice Owunne <93526806+Euniccodes@users.noreply.github.com>
2023-01-11 16:41:04 +0100

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,97 +1,49 @@
 #include "hash_tables.h"
 
-
-
 void print_node(hash_node_t *n);
 
-
-
 /**
-
  * hash_table_print - prints a hash table
-
  * @ht: the table
-
  */
-
-
 
 void hash_table_print(const hash_table_t *ht)
-  
 {
-  
-  unsigned long int i = 0;
-  
-  int comma = 0;
-  
+	unsigned long int i = 0;
+	int comma = 0;
 
-  
-  if (!ht)
-    
-    return;
-  
-  printf("{");
-  
-  while (i < ht->size)
-    
-    {
-      
-      if (ht->array[i])
-	
+	if (!ht)
+		return;
+	printf("{");
+	while (i < ht->size)
 	{
-	  
-	  if (comma)
-	    
-	    {
-	      
-	      printf(", ");
-	      
-	      comma = 0;
-	      
-	    }
-	  
-	  print_node(ht->array[i]);
-	  
-	  comma = 1;
-	  
+		if (ht->array[i])
+		{
+			if (comma)
+			{
+				printf(", ");
+				comma = 0;
+			}
+			print_node(ht->array[i]);
+			comma = 1;
+		}
+		i++;
 	}
-      
-      i++;
-      
-    }
-  
-  printf("}\n");
-  
+	printf("}\n");
 }
 
-
-
 /**
-
  * print_node - prints a node and all its linked nodes
-
  * @n: node pointer
-
  */
-
 void print_node(hash_node_t *n)
-  
 {
-  
-  if (!n)
-    
-    return;
-  
-  printf("'%s': '%s'", n->key, n->value);
-  
-  if (n->next)
-    
-    {
-      
-      printf(", ");
-      
-      print_node(n->next);
-      
-    }
-  
+	if (!n)
+		return;
+	printf("'%s': '%s'", n->key, n->value);
+	if (n->next)
+	{
+		printf(", ");
+		print_node(n->next);
+	}
 }
